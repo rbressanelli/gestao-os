7deb00be2c7a2f07567b78c1f22b262e569f5480
Roberto Bressanelli <rbressanelli@gmail.com>
2026-02-25 15:07:04 -0300

    created comment model'

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=32, null=False)
+    description = models.TextField(max_length=512)
+    
+    
+    def __str__(self):
+        return self.name
+
