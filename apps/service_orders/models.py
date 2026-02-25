0ddd41900dca36fb265a3210b34facf943cf63b2
Roberto Bressanelli <rbressanelli@gmail.com>
2026-02-25 15:36:03 -0300

    Feat: implemented service_orders model

--- apps/service_orders/models.py
+++ apps/service_orders/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class ServiceOrder(models.Model):
+    
+    PRIORITY_CHOICES = (
+        ('low', 'Baixa'),
+        ('medium', 'Média'),
+        ('high', 'Alta')
+    )
+    
+    STATUS_CHOICES = (
+        ('opened', 'Aberta'),
+        ('in_progress', 'Em andamento'),
+        ('concluded', 'Concluída'),
+        ('canceled', 'Cancelada')
+    )
+    
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    os_number = models.CharField(max_length=20, null=False)
+    problem_description = models.TextField(max_length=1024, null=False)
+    status = models.CharField(choices=STATUS_CHOICES, default='opened')
+    priority = models.CharField(choices=PRIORITY_CHOICES, default='low')
+    created_date = models.DateTimeField(auto_now_add=True)
+    closed_date = models.DateTimeField(null=False)
+    
+    
+    def __str__(self):
+        return f'{
+            "OS": {self.os_number},
+            "Prioridade": {self.priority},
+            "Status": {self.status},
+            "Descrição": {self.problem_description}
+            }'
+
