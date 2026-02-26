1458a6709bf7f3c95a5e98d90111ed4df8d36208
Roberto Bressanelli <rbressanelli@gmail.com>
2026-02-26 15:53:51 -0300

    Feat: implemented serializer, views and routes to technician

---
+++ apps/technicians/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Technician
+
+
+class TechnicianSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technician
+        fields = '__all__'
+    
+    
+    def create(self, validated_data):
+        return Technician.objects.create(**validated_data)
+    
+    def update(self, instance: Technician, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name) 
+        instance.last_name = validated_data.get("last_name", instance.last_name) 
+        instance.specialty = validated_data.get("specialty", instance.specialty)
+        instance.email = validated_data.get("email", instance.email)
+        instance.active = validated_data.get("active", instance.active)
+        instance.hiring_date = validated_data.get("hiring_date", instance.hiring_date)        
+        instance.save()
+        return instance
--- apps/technicians/urls.py
+++ apps/technicians/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from apps.technicians import views
 
 app_name = 'technicians'
+
 urlpatterns = [
-    path('technicians/', views.Dummy.as_view())
+    path('technicians/<str:technician_id>/', views.TechnicianView.as_view()),
+    path('technicians/', views.TechnicianView.as_view())
 ]
